3c0b3557afd41f5616dccb9ee066507e836c0e3e
sova <mahatoshobha143@gmail.com>
2023-12-07 12:27:56 +0545

    homepage ui editing

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -132,152 +132,180 @@ class _HomePageState extends State<HomePage> {
                 TextStyle(fontFamily: "Cera Pro", fontWeight: FontWeight.bold),
           ),
         ),
-        body: SingleChildScrollView(
-            child: Column(
-          children: [
-            const SizedBox(
-              height: 10,
-            ),
-            // assistant image
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: RadialGradient(
+                transform: GradientRotation(3),
+                center: Alignment.topRight,
+                 radius: 3,
+               // tileMode: TileMode.clamp,
+                colors: [
+            //  Pallete.color1,
+                Pallete.color2,
+               // Pallete.color2,
+                Pallete.color3,
+              //  Pallete.color4,
+                 Pallete.color5,
+                 Pallete.color6,
+              //  Pallete.color7,
+             // Pallete.color8,
+              Pallete.color9,
 
-            Stack(
-              children: [
-                Center(
-                  child: Container(
-                    height: 120,
-                    width: 120,
-                    decoration: const BoxDecoration(
-                        color: Pallete.assistantCircleColor,
-                        shape: BoxShape.circle),
+              //  Pallete.color10,
+
+              // Pallete.whiteColor,
+
+              // Pallete.color4,
+              // Pallete.color10,
+              // Pallete.whiteColor,
+            ])),
+          child: SingleChildScrollView(
+              child: Column(
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              // assistant image
+          
+              Stack(
+                children: [
+                  Center(
+                    child: Container(
+                      height: 120,
+                      width: 120,
+                      decoration: const BoxDecoration(
+                          color: Pallete.assistantCircleColor,
+                          shape: BoxShape.circle),
+                    ),
                   ),
-                ),
-                Container(
-                  height: 129,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: AssetImage(
-                        "assets/images/voiceAssistant.png",
+                  Container(
+                    height: 129,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage(
+                          "assets/images/voiceAssistant.png",
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-
-            //chat bubble
-
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-              margin:
-                  const EdgeInsets.symmetric(horizontal: 35).copyWith(top: 10),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Pallete.borderColor),
-                  borderRadius:
-                      BorderRadius.circular(20).copyWith(topLeft: Radius.zero)),
-              child:  Text(generatedContent == null
-                    ? "Good Morning ! what task can i do for you?"
-                    : generatedContent!,
-                style: TextStyle(fontFamily: "Cera Pro", fontSize:generatedContent == null ? 25 : 18),
-              ),
-            ),
-
-            //here are new features text
-
-            Visibility(
-              visible: generatedContent==null,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20, top: 15),
-                child: Container(
-                  alignment: Alignment.centerLeft,
-                  child: const Text(
-                    "Here are new features",
-                    style: TextStyle(
-                        fontFamily: "Cera Pro",
-                        fontSize: 20,
-                        color: Pallete.mainFontColor,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-            ),
-            // //features box 2ota
-            Visibility(
-              visible: generatedContent==null,
-              child: const Column(
-                children: [
-                   FeatureBox(
-                      headerText: "Chat GPT",
-                      descriptionText:
-                          "A smarter way to stay organized with chat GPT",
-                      color: Pallete.firstSuggestionBoxColor),
-                       FeatureBox(
-                  headerText: "Voice Assistant ",
-                  descriptionText:
-                      "Get the best of worlds with a voice assistant powered by ChatGpt.",
-                  color: Pallete.secondSuggestionBoxColor),
                 ],
               ),
-            ),
-
-            
-
-            
-            //textfield
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Pallete.assistantCircleColor),
-              child: Row(children: [
-                Expanded(
-                    child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    controller: textController,
-                    decoration: const InputDecoration(
-                        hintText: "Message", border: InputBorder.none),
+          
+              //chat bubble
+          
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 35).copyWith(top: 10),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Pallete.borderColor),
+                    borderRadius:
+                        BorderRadius.circular(20).copyWith(topLeft: Radius.zero)),
+                child:  Text(generatedContent == null
+                      ? "Good Morning ! what task can i do for you?"
+                      : generatedContent!,
+                  style: TextStyle(fontFamily: "Cera Pro", fontSize:generatedContent == null ? 25 : 18),
+                ),
+              ),
+          
+              //here are new features text
+          
+              Visibility(
+                visible: generatedContent==null,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20, top: 15),
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    child: const Text(
+                      "Here are new features",
+                      style: TextStyle(
+                          fontFamily: "Cera Pro",
+                          fontSize: 20,
+                          color: Pallete.mainFontColor,
+                          fontWeight: FontWeight.bold),
+                    ),
                   ),
-                )),
-                GestureDetector(
-                    onTap: () async {
-                      if (await speechToText.hasPermission &&
-                          speechToText.isNotListening) {
-                        await startListening();
-                      } else if (speechToText.isListening) {
-                        final speech =
-                            await openAIService.chatGPTAPI(lastWords);
-                        await systemSpeak(speech);
-                        // print(lastWords);
-                        generatedContent = speech;
-                        setState(() {});
-                        await stopListening();
-                      }else{
-                        initSpeechToText();
-                      }
-                    },
-                    child: const Icon(Icons.mic)),
-                GestureDetector(
-                  onTap: () async {
-                    final speech =
-                        await openAIService.chatGPTAPI(textController.text);
-                    await systemSpeak(speech);
-                    generatedContent = speech;
-                    textController.clear();
+                ),
+              ),
+              // //features box 2ota
+              Visibility(
+                visible: generatedContent==null,
+                child: const Column(
+                  children: [
+                     FeatureBox(
+                        headerText: "Chat GPT",
+                        descriptionText:
+                            "A smarter way to stay organized with chat GPT",
+                        color: Pallete.firstSuggestionBoxColor),
+                         FeatureBox(
+                    headerText: "Voice Assistant ",
+                    descriptionText:
+                        "Get the best of worlds with a voice assistant powered by ChatGpt.",
+                    color: Pallete.secondSuggestionBoxColor),
+                  ],
+                ),
+              ),
+          
               
-                    setState(() {});
-                  },
-                  child: const Padding(
-                    padding: EdgeInsets.all(14.0),
-                    child:  Icon(
-                      Icons.send,
-                      size: 35,
-                      color: Colors.black,
-                    ),)
-                  ),
-              ]),
-            )
-          ],
-        )),
+          
+              
+              //textfield
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Pallete.assistantCircleColor),
+                child: Row(children: [
+                  Expanded(
+                      child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      controller: textController,
+                      decoration: const InputDecoration(
+                          hintText: "Message", border: InputBorder.none),
+                    ),
+                  )),
+                  GestureDetector(
+                      onTap: () async {
+                        if (await speechToText.hasPermission &&
+                            speechToText.isNotListening) {
+                          await startListening();
+                        } else if (speechToText.isListening) {
+                          final speech =
+                              await openAIService.chatGPTAPI(lastWords);
+                          await systemSpeak(speech);
+                          // print(lastWords);
+                          generatedContent = speech;
+                          setState(() {});
+                          await stopListening();
+                        }else{
+                          initSpeechToText();
+                        }
+                      },
+                      child: const Icon(Icons.mic)),
+                  GestureDetector(
+                    onTap: () async {
+                      final speech =
+                          await openAIService.chatGPTAPI(textController.text);
+                      await systemSpeak(speech);
+                      generatedContent = speech;
+                      textController.clear();
+                
+                      setState(() {});
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.all(14.0),
+                      child:  Icon(
+                        Icons.send,
+                        size: 35,
+                        color: Colors.black,
+                      ),)
+                    ),
+                ]),
+              )
+            ],
+          )),
+        ),
       ),
     );
   }
