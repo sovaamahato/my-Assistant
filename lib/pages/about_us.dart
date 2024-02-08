b8670413631b5a906ff631602f6d4e325f99faae
sova <mahatoshobha143@gmail.com>
2024-02-08 23:05:42 +0545

    edited about us page

--- lib/pages/about_us.dart
+++ lib/pages/about_us.dart
@@ -41,69 +41,104 @@ class AboutUs extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Spacer(),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.6,
-                  color: const Color.fromARGB(255, 145, 174, 243),
-                  child: Column(
-                    children: [
-                      const Text(
-                        'Developed by',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'Upasana Khatiwada',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              final Uri url = Uri.parse(
-                                  'https://github.com/upasana-khatiwada');
-                              launchUrl(url);
-                            },
-                            child: Image.asset(
-                              'assets/images/GitHub-Symbol.png',
-                              width: 40,
-                              height: 40,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 65, 89, 144),
+                        borderRadius: BorderRadius.circular(15)),
+                    padding: const EdgeInsets.all(10),
+                    width: MediaQuery.of(context).size.width * 0.96,
+                    child: Column(
+                      children: [
+                        const Text(
+                          'Developed by',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  'Upasana \nKhatiwada',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    final Uri url = Uri.parse(
+                                        'https://github.com/upasana-khatiwada');
+                                    launchUrl(url);
+                                  },
+                                  child: Image.asset(
+                                    'assets/images/GitHub-Symbol.png',
+                                    width: 40,
+                                    height: 40,
+                                  ),
+                                ),
+                              ],
                             ),
-                          )
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text(
-                            'Sova Kushwaha',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              final Uri url =
-                                  Uri.parse('https://github.com/sovaamahato');
-                              launchUrl(url);
-                            },
-                            child: Image.asset(
-                              'assets/images/GitHub-Symbol.png',
-                              width: 40,
-                              height: 40,
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Text(
+                                  'Sova\n Kushwaha',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    final Uri url = Uri.parse(
+                                        'https://github.com/sovaamahato');
+                                    launchUrl(url);
+                                  },
+                                  child: Image.asset(
+                                    'assets/images/GitHub-Symbol.png',
+                                    width: 40,
+                                    height: 40,
+                                  ),
+                                )
+                              ],
                             ),
-                          )
-                        ],
-                      ),
-                    ],
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                const Spacer(
-                  flex: 2,
+                const Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Divider(
+                    color: Colors.deepPurple,
+                    thickness: 2,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
                 ),
                 const Text(
                   "Developed using flutter. \n SparkyBot\n Version 1.0.1",
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 56, 19, 19), fontSize: 15),
                 ),
-                const Spacer()
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Divider(
+                    color: Colors.deepPurple,
+                    thickness: 2,
+                  ),
+                ),
+                const Spacer(),
               ],
             ),
           )),
--- lib/secret.dart
+++ lib/secret.dart
@@ -1,3 +1,3 @@
 //const OpenAIAPIKey='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';
 
-const OpenAIAPIKey = '';
+const OpenAIAPIKey = "";
