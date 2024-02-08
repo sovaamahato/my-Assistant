339cd0e9fae624bf7ea9af885bf008abf4d8ccd4
sova <mahatoshobha143@gmail.com>
2024-02-08 16:11:58 +0545

    some changes

--- lib/secret.dart
+++ lib/secret.dart
@@ -1,3 +1,1 @@
-//const OpenAIAPIKey='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';
-
-const OpenAIAPIKey = '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';
+const OpenAIAPIKey = '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';
